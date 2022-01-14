f3125bdfb3e0319e39f4ae2e2e8272ccf910da4d
adbenitez <asieldbenitez@gmail.com>
2022-01-14 03:53:27 -0500

    try to avoid loading the board before document size is determined

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
+DrawingBoard.Control.Lock = DrawingBoard.Control.extend({
+    name: 'lock',
+    initialize: function() {
+	this.$el.append('<button class="drawing-board-control-download-button"></button>');
+	this.$el.on('click', '.drawing-board-control-download-button', $.proxy(function(e) {
+            window.webxdc.sendUpdate(this.board.getImg(), window.webxdc.selfName() + ' sent a draw');
+	    e.preventDefault();
+	}, this));
+    }
+});
+
 onload = () => {
     window.webxdc.setUpdateListener((update) => {
         setImg(update.payload);
@@ -6,27 +17,18 @@ onload = () => {
     if (update) {
         setImg(update.payload);
     } else {
-        DrawingBoard.Control.Lock = DrawingBoard.Control.extend({
-
-            name: 'lock',
-
-            initialize: function() {
-	        this.$el.append('<button class="drawing-board-control-download-button"></button>');
-	        this.$el.on('click', '.drawing-board-control-download-button', $.proxy(function(e) {
-                    window.webxdc.sendUpdate(this.board.getImg(), window.webxdc.selfName() + ' sent a draw');
-	            e.preventDefault();
-	        }, this));
-            }
-
-        });
-
-        const board = new DrawingBoard.Board('board', {
-            webStorage: false
-        });
-        board.addControl('Lock');
+        setTimeout(initBoard, 200);
     }
-}
+};
 
 setImg = (url) => {
     document.getElementById('board').innerHTML = '<img style="max-width:100%;height:auto" src="' + url +'">';
-}
+};
+
+initBoard = () => {
+    const board = new DrawingBoard.Board('board', {
+        webStorage: false,
+        errorMessage: 'Failed to load the editor',
+    });
+    board.addControl('Lock');
+};
