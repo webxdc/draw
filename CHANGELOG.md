94ec6e41a4f223ff771f98075949745ad666a7e4
adbenitez <asieldbenitez@gmail.com>
2022-02-13 01:44:09 -0500

    bundle sources in a single file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,8 @@
 # Changelog
 
-## 0.2.0
+## 0.3.0
 
 - initial release
 
 
-[Unreleased]: https://github.com/adbenitez/draw.xdc/compare/v0.2.0...HEAD
+[Unreleased]: https://github.com/adbenitez/draw.xdc/compare/v0.3.0...HEAD
--- js/index.js
+++ js/index.js
@@ -1,15 +1,3 @@
-DrawingBoard.Control.Lock = DrawingBoard.Control.extend({
-    name: 'lock',
-    initialize: function() {
-	this.$el.append('<button class="drawing-board-control-download-button"></button>');
-	this.$el.on('click', '.drawing-board-control-download-button', $.proxy(function(e) {
-            const desc = window.webxdc.selfName + ' sent a draw';
-            window.webxdc.sendUpdate({payload: this.board.getImg(), summary: desc}, desc);
-	    e.preventDefault();
-	}, this));
-    }
-});
-
 initBoard = () => {
     $('#board').html('');
     new DrawingBoard.Board('board', {
@@ -26,6 +14,19 @@ onload = () => {
     window.webxdc.setUpdateListener((update) => {
         setImg(update.payload);
     });
+
+    DrawingBoard.Control.Lock = DrawingBoard.Control.extend({
+        name: 'lock',
+        initialize: function() {
+	    this.$el.append('<button class="drawing-board-control-download-button"></button>');
+	    this.$el.on('click', '.drawing-board-control-download-button', $.proxy(function(e) {
+                const desc = window.webxdc.selfName + ' sent a draw';
+                window.webxdc.sendUpdate({payload: this.board.getImg(), summary: desc}, desc);
+	        e.preventDefault();
+	    }, this));
+        }
+    });
+
     window.webxdc.getAllUpdates().then((updates) => {
         if (updates[0]) {
             setImg(updates[0].payload);
