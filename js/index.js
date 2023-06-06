f942e4f2780fd0bef75262a03a55e3f8c80d2a98
adbenitez <asieldbenitez@gmail.com>
2023-06-06 19:59:34 +0200

    use new webxdc APIs (sendToChat and importFiles)

--- js/index.js
+++ js/index.js
@@ -1,21 +1,49 @@
-onload = () => {
-    DrawingBoard.Control.Lock = DrawingBoard.Control.extend({
-        name: 'lock',
-        initialize: function() {
-	    this.$el.append('<button class="drawing-board-control-download-button"></button>');
-	    this.$el.on('click', '.drawing-board-control-download-button', $.proxy(function(e) {
-                const desc = window.webxdc.selfName + ' sent a draw';
-                window.webxdc.sendUpdate({payload: this.board.getImg(), summary: desc}, desc);
+DrawingBoard.Control.Menu = DrawingBoard.Control.extend({
+    name: 'menu',
+    initialize: function() {
+	    this.$el.append('<button id="menu-btn"></button><div id="dropdown-content"><a href="#" id="import-menuitem">Open...</a><a href="#" id="share-menuitem">Share</a></div>');
+	    this.$el.on('click', '#menu-btn', function(e) {
+            document.getElementById("dropdown-content").classList.toggle("show");
+	        e.preventDefault();
+	    });
+        // Close the dropdown menu if the user clicks outside of it
+        window.onclick = function(event) {
+            if (!event.target.matches('#menu-btn')) {
+                var openDropdown = document.getElementById("dropdown-content");
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+
+	    this.$el.on('click', '#import-menuitem', $.proxy(function(e) {
+            const board = this.board;
+            webxdc.importFiles({mimeTypes: ['image/*']}).then((files) => {
+                var reader = new FileReader();
+                reader.onload = function() {
+                    board.opts.background = reader.result;
+                    board.reset({background: true, history: false});
+                    board.initHistory();
+                    board.ev.trigger('historyNavigation');
+                };
+                reader.readAsDataURL(files[0]);
+            });
 	        e.preventDefault();
 	    }, this));
-        }
-    });
-    new DrawingBoard.Board('board', {
-        webStorage: false,
-        errorMessage: 'Failed to load the editor',
-    }).addControl('Lock');
 
-    window.webxdc.setUpdateListener((update) => {
-        $('#board').html('<img style="max-width:100%;height:auto" src="' + update.payload +'">');
-    }, 0);
+	    this.$el.on('click', '#share-menuitem', $.proxy(function(e) {
+            var b64Data = this.board.getImg().split(',')[1];
+            window.webxdc.sendToChat({file: {name: "draw.png", base64: b64Data}});
+	        e.preventDefault();
+	    }, this));
+    }
+});
+
+onload = () => {
+    window.board = new DrawingBoard.Board('board', {
+        webStorage: false,
+        errorMessage: 'Failed to load',
+        stretchImg: true,
+    });
+    window.board.addControl('Menu');
 };
